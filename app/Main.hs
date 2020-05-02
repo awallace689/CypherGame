8c6f5f820840f7823e04780a2056965faf79f88c
Adam Wallace <awallace689@gmail.com>
2020-05-01 22:48:26 -0500

    rando generation immplemented

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,8 @@
 {- Map operation to list with random number 1-9 (2-9 for mult) 1-3 times depending on difficulty selected. -}
-import System.Random
-import Text.Read
-import Data.List
+import System.Random (newStdGen, randomRs)
+import Text.Read (readMaybe)
+import Data.Text (pack, strip, Text)
+import System.Exit (exitSuccess)
 
 title =
   "  ___  _  _  ____  _   _  ____  ____\n" ++
@@ -11,6 +12,8 @@ title =
 
 invalidWarn = "Invalid input. Try again...\n\n"
 
+seedGenMenu = "Enter 'q' at any time to quit.\n\nSelect seed type:\n1) [1, 2, 3]\n2) Random\n> "
+
 data Cypher = Div Int Cypher -- `div`
             | Mul Int Cypher -- *
             | Sub Int Cypher -- -
@@ -18,14 +21,15 @@ data Cypher = Div Int Cypher -- `div`
             | Seed [Int]
               deriving Show
 
-randomSeed :: [Int] -> [Random Int]
-randomSeed = randomSeedGo [] (randomRs (-9 :: Int, 9) newStdGen)
+randomSeed :: IO Cypher
+randomSeed = newStdGen >>= \ g -> randomSeedGo ([] :: [Int]) (randomRs (-9, 9) g)
 
+randomSeedGo :: [Int] -> [Int] -> IO Cypher
 randomSeedGo xs rs = if length xs == 3 
-                     then xs 
+                     then return $ Seed xs 
                      else if not $ elem r xs
-                          then randomSeedGo r:xs $ tail rs
-                          else randomSeedGo xs $ tail rs
+                          then randomSeedGo (r:xs) (tail rs)
+                          else randomSeedGo (xs) (tail rs)
                             where r = head rs
 
 parseInt :: IO String -> IO Int
@@ -34,23 +38,32 @@ parseInt inp = inp >>= \ inp -> return (read inp :: Int)
 intGuard :: String -> Bool
 intGuard x  = let res = readMaybe x :: Maybe Int
               in case res of
-                Just x -> True
-                Nothing -> False
+                   Just x -> True
+                   Nothing -> False
+
+intQuitGuard :: String -> Bool
+intQuitGuard x = let res = readMaybe x :: Maybe Int
+                 in case res of
+                      Just x  -> True
+                      Nothing -> False
 
 prompt :: String -> IO String
 prompt p = putStr p >> getLine
 
 promptGuard :: String -> (String -> Bool) -> String -> IO String
-promptGuard p f w = (putStr p) >> getLine >>= \ inp -> if f inp
-                                                     then return inp
-                                                     else putStr w >> promptGuard p f w
+promptGuard p f w = (putStr p) >> 
+                    getLine    >>= \ inp -> if (strip $ pack inp) == (pack "q")
+                                            then exitSuccess
+                                            else if f inp
+                                                 then return inp
+                                                 else putStr w >> promptGuard p f w
                     
 getSeed :: IO Cypher
 getSeed = do
-  inp <- parseInt $ promptGuard "Input '1'.\n> " intGuard invalidWarn
+  inp <- parseInt $ promptGuard seedGenMenu intGuard invalidWarn
   case inp of
     1 -> return $ Seed [1, 2, 3]
-    2 -> return $ randomSeed
+    2 -> randomSeed
     _ -> do
       putStr invalidWarn
       getSeed
