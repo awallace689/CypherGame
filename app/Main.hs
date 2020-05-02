d4670e9ae8878651d32f844b394447c8a6df5a40
Adam Wallacessh-keygen -t rsa -b 4096 -C awallace689@gmail.com <awallace689@gmail.com>
2020-05-01 21:08:54 -0500

    failed at random # gen

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,7 @@
 {- Map operation to list with random number 1-9 (2-9 for mult) 1-3 times depending on difficulty selected. -}
 import System.Random
 import Text.Read
+import Data.List
 
 title =
   "  ___  _  _  ____  _   _  ____  ____\n" ++
@@ -8,14 +9,24 @@ title =
   "( (__  \\  /  )___/ ) _ (  )__)  )   /\n" ++
   " \\___) (__) (__)  (_) (_)(____)(_)\\_)\n\n\n"                         
 
-invalidWarn = "Invalid input. Try again...\n"
+invalidWarn = "Invalid input. Try again...\n\n"
 
 data Cypher = Div Int Cypher -- `div`
             | Mul Int Cypher -- *
             | Sub Int Cypher -- -
             | Add Int Cypher -- +
-            | Seed [Int] 
+            | Seed [Int]
               deriving Show
+
+randomSeed :: [Int] -> [Random Int]
+randomSeed = randomSeedGo [] (randomRs (-9 :: Int, 9) newStdGen)
+
+randomSeedGo xs rs = if length xs == 3 
+                     then xs 
+                     else if not $ elem r xs
+                          then randomSeedGo r:xs $ tail rs
+                          else randomSeedGo xs $ tail rs
+                            where r = head rs
 
 parseInt :: IO String -> IO Int
 parseInt inp = inp >>= \ inp -> return (read inp :: Int)
@@ -29,21 +40,22 @@ intGuard x  = let res = readMaybe x :: Maybe Int
 prompt :: String -> IO String
 prompt p = putStr p >> getLine
 
-promptGuard :: String -> (String -> Bool) -> IO String
-promptGuard p f = (putStr p) >> getLine >>= \ inp -> if f inp
+promptGuard :: String -> (String -> Bool) -> String -> IO String
+promptGuard p f w = (putStr p) >> getLine >>= \ inp -> if f inp
                                                      then return inp
-                                                     else putStr invalidWarn >> promptGuard p f
+                                                     else putStr w >> promptGuard p f w
                     
-getSeed :: IO Int -> IO Cypher
-getSeed input = input >>= \ x -> case x of 
-                                      1 -> return $ Seed [1, 2, 3]
-                                      --2 -> return genRandomSeed
-                                      _ -> do
-                                        putStr invalidWarn
-                                        getSeed $ parseInt $ promptGuard "> " intGuard
+getSeed :: IO Cypher
+getSeed = do
+  inp <- parseInt $ promptGuard "Input '1'.\n> " intGuard invalidWarn
+  case inp of
+    1 -> return $ Seed [1, 2, 3]
+    2 -> return $ randomSeed
+    _ -> do
+      putStr invalidWarn
+      getSeed
 
 main = do
   putStr title
-  putStr "Input '1'.\n"
-  seed <- getSeed $ parseInt $ promptGuard "> " intGuard 
+  seed <- getSeed
   putStr $ "Encoding: " ++ (show seed) ++ "\n"
